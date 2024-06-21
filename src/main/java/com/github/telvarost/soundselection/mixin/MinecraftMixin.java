c8dbc682a2ac6a8e5227f098aa7b8fa95adc9d12
telontelvarost <telontelvarost@gmail.com>
2024-06-21 18:16:13 -0400

    Add ability to disable other sounds

--- src/main/java/com/github/telvarost/soundselection/mixin/MinecraftMixin.java
+++ src/main/java/com/github/telvarost/soundselection/mixin/MinecraftMixin.java
@@ -1,5 +1,6 @@
 package com.github.telvarost.soundselection.mixin;
 
+import com.github.telvarost.soundselection.ModHelper;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
@@ -18,12 +19,42 @@ public class MinecraftMixin {
             method = "loadResource",
             at = @At(
                     value = "INVOKE",
+                    target = "Lnet/minecraft/client/sound/SoundManager;loadSound(Ljava/lang/String;Ljava/io/File;)V"
+            ),
+            cancellable = true
+    )
+    public void loadSoundFromDirSound(String file, File par2, CallbackInfo ci) {
+        if (!ModHelper.ModHelperFields.soundPack.isBlank()) {
+            System.out.println("Removing sound: " + file);
+            ci.cancel();
+        }
+    }
+
+    @Inject(
+            method = "loadResource",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/sound/SoundManager;loadStreaming(Ljava/lang/String;Ljava/io/File;)V"
+            ),
+            cancellable = true
+    )
+    public void loadSoundFromDirStreaming(String file, File par2, CallbackInfo ci) {
+        if (!ModHelper.ModHelperFields.soundPack.isBlank()) {
+            System.out.println("Removing streaming sound: " + file);
+            ci.cancel();
+        }
+    }
+
+    @Inject(
+            method = "loadResource",
+            at = @At(
+                    value = "INVOKE",
                     target = "Lnet/minecraft/client/sound/SoundManager;loadMusic(Ljava/lang/String;Ljava/io/File;)V"
             ),
             cancellable = true
     )
-    public void loadSoundFromDir(String file, File par2, CallbackInfo ci) {
-        if (true) {
+    public void loadSoundFromDirMusic(String file, File par2, CallbackInfo ci) {
+        if (!ModHelper.ModHelperFields.soundPack.isBlank()) {
             System.out.println("Removing default Minecraft background music.");
             ci.cancel();
         }
