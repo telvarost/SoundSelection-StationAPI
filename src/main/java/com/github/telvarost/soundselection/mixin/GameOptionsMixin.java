cf77e852279816cbb9c8b1ea92586d665e795721
telvarost <telontelvarost@gmail.com>
2025-06-07 19:41:15 -0400

    Remove redirect to make mod more compatible

--- src/main/java/com/github/telvarost/soundselection/mixin/GameOptionsMixin.java
+++ src/main/java/com/github/telvarost/soundselection/mixin/GameOptionsMixin.java
@@ -1,13 +1,14 @@
 package com.github.telvarost.soundselection.mixin;
 
 import com.github.telvarost.soundselection.ModHelper;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.option.GameOptions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.PrintWriter;
@@ -21,15 +22,15 @@ public class GameOptionsMixin {
         ModHelper.loadSoundPack(false);
     }
 
-    @Redirect(
+    @WrapOperation(
             method = "save",
             at = @At(
                     value = "INVOKE",
                     target = "Ljava/io/PrintWriter;close()V"
             )
     )
-    public void saveOptions(PrintWriter instance) {
+    public void saveOptions(PrintWriter instance, Operation<Void> original) {
         instance.println("soundPack:" + ModHelper.ModHelperFields.soundPack);
-        instance.close();
+        original.call(instance);
     }
 }
